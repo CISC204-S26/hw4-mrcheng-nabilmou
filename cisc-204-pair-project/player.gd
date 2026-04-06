ba49782f8208d6f18a96d701ffcbd784e3de3b44
mcheng11 <mcheng0439@gmail.com>
2026-04-06 14:36:59 -0400

    player

---
+++ cisc-204-pair-project/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from project settings to stay consistent
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	# Add gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get input direction (-1, 0, 1)
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide() # Handles collisions and slopes automatically
