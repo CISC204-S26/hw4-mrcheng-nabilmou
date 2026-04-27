f33ceeead029be77d3ce24bb3a38a4a11af28ffd
Nabil Moustafa <nabilmou@udel.edu>
2026-04-26 23:49:26 -0400

    should be done now

--- cisc-204-pair-project/SCRIPTS/ending_title_screen.gd
+++ cisc-204-pair-project/SCRIPTS/ending_title_screen.gd
@@ -2,16 +2,31 @@ extends CanvasLayer
 
 @onready var main_menu_button = $VBoxContainer/MainMenu
 @onready var quit_button = $VBoxContainer/Quit
+@onready var hover_sound = $HoverSound
+@onready var click_sound = $ClickSound
+
 
 @export var main_menu_scene: String = "res://SCENES/UI_SCREENS/MenuUI.tscn"
 
+
 func _ready():
-	main_menu_button.pressed.connect(_on_main_menu_pressed)
-	quit_button.pressed.connect(_on_quit_pressed)
+	pass
+
 
 func _on_main_menu_pressed():
 	await SceneChanger.fade_out()
 	get_tree().change_scene_to_file(main_menu_scene)
 
+
 func _on_quit_pressed():
+	await get_tree().create_timer(.5).timeout
 	get_tree().quit()
+
+
+# ------------------------ SOUNDS SOUNDS SOUNDS -----------------------------------------
+func play_hover_sound():
+	hover_sound.play()
+
+
+func play_click_sound():
+	click_sound.play()
